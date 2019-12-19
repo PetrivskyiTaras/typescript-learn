b34cdb0e9814e0671a5f2b55686e873f7689866e
tapet <tapet@ciklum.com>
2019-12-19 16:43:04 +0200

    some commit

---
+++ features/anotations/variables.ts
@@ -0,0 +1,14 @@
+const apples: number = 5;
+let time: Date;
+time = new Date();
+
+let colors: string[] = ['red', 'green'];
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 30
+};
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
