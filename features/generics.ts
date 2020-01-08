814672fb652c80a35c850f5d6e5d23926761b028
taraspetrivskyi <taras_pv@list.ru>
2020-01-08 23:38:05 +0200

    generics overview

---
+++ features/generics.ts
@@ -0,0 +1,53 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+}
+
+function print<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+}
+
+print<string>(['a', 'b', 'c']);
+
+interface Printable {
+  print(): void;
+}
+
+function printHouseOrCar<T extends Printable>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
