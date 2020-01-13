eaf8bdfd90a44f8aabf41c9af0cd3c1682391ce0
taraspetrivskyi <taras_pv@list.ru>
2020-01-13 19:33:55 +0200

    web framework part of course, break before react part

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello from ts');
+import { User } from './models/User';
+
+const user = new User({ name: 'userName', age: 33 });
+
+user.on('change', () => {
+  console.log('My name is name!');
+});
+
+user.trigger('change');
